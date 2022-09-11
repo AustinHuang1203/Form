e3960c4c2ed1089432a28c1bd8eac60089983f7d
austin <austinhuang1203@gmail.com>
2022-09-11 18:47:21 +0800

    added password validation

--- code.js
+++ code.js
@@ -1,9 +1,23 @@
 const pwd = document.getElementById("password");
 pwd.addEventListener("mouseout",store);
-
+pwd.addEventListener("keyup",store);
+pwd.addEventListener("mouseout",check);
+pwd.addEventListener("keyup",check);
+const pwd1 = document.getElementById("password1");
+pwd1.addEventListener("mouseout",check);
+pwd1.addEventListener("keyup",check);
 
 let stored = ""
 function store(){
     stored = pwd.value;
+}
+
+function check(){
+    if (stored == pwd1.value){
+        pwd1.setCustomValidity("");
+    }
+    else{
+        pwd1.setCustomValidity("Passwords do not match!");
+    }
 }
 
